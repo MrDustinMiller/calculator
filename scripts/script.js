10876746f64f219d2efb3765ce0fbbfd3672a2d5
MrDustinMiller <dustin.miller@mscoding.org>
2022-11-11 19:46:52 -0600

    added operate, test, clear, and display functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,52 +1,97 @@
 let display = document.querySelector(".display");
-let numbers = document.querySelectorAll(".numbers");
+let numbers = document.querySelectorAll(".num");
 let operations = document.querySelectorAll(".operations");
 let deleteBtns = document.querySelectorAll(".delete-buttons");
-let operation;
+let operatorClicked = false;
+let operator;
+let numOne = 0;
+let numTwo = 0;
+let result = 0;
 
 let clearScreen = () => {
+  //issue with clearing data. its setting and keeping numone and numtwo at 0
+  //not updating when its clicked next
   display.value = "";
+  numOne = 0;
+  numTwo = 0;
+  result = 0;
+  operator = "";
 };
 
 let backSpace = () => {
-  //problem here with the dislplay value not updating
-  //literally dont think ive gotten there yet tho
   let array = Array.from(display.value);
-
   array.pop();
 
-  //fix this, convert from array w/no commas or nething
-  display.value = array.toString();
+  let output = array.join("");
+  display.value = output.toString();
 
   if (array.length === 0) {
     display.value = "";
   }
-  console.log(array);
-  return array;
 };
 
 let addition = (numOne, numTwo) => {
-  return numOne + numTwo;
+  result = Number(numOne) + Number(numTwo);
 };
 
 let subtraction = (numOne, numTwo) => {
-  return numOne - numTwo;
+  result = Number(numOne) - Number(numTwo);
 };
 
 let multiplication = (array) => {
-  return array.reduce((accumulator, nextItem) => accumulator * nextItem);
+  //   let nums = Array.from(array);
+  //   let result = nums.reduce((accumulator, nextItem) => accumulator * nextItem);
+  //   console.log(array);
+  //   display.value = result;
 };
 
 let division = () => {};
 
-let operate = () => {
-  //switch/case
+let operate = (operator, numOne, numTwo) => {
+  switch (operator) {
+    case "+":
+      console.log(operator, numOne, numTwo);
+      addition(numOne, numTwo);
+      break;
+    case "-":
+      subtraction(numOne, numTwo);
+      break;
+    case "*":
+      multiplication(numOne, numTwo);
+      break;
+    case "/":
+      division(numOne, numTwo);
+      break;
+
+    default:
+      alert("No valid operator selected.");
+      break;
+  }
 };
+
+function test() {
+  console.log(numOne, numTwo);
+  if (numOne && numTwo > 0) {
+    operate(operator, numOne, numTwo);
+  }
+}
 
 numbers.forEach((element) => {
   element.addEventListener("click", (e) => {
     display.value += e.target.value; //plus operation
-    console.log(e.target.value);
+
+    if (operatorClicked != true) {
+      numOne += e.target.value; //issue with zerores being in front
+    }
+
+    if (numOne > 0 && operatorClicked != false) {
+      display.value = numOne + `${operator}`;
+      numTwo += e.target.value;
+      display.value = numOne + `${operator}` + numTwo;
+    }
+
+    test();
+    //console.log(e.target.value);
   });
 });
 
@@ -62,6 +107,16 @@ deleteBtns.forEach((element) => {
 
 operations.forEach((element) => {
   element.addEventListener("click", (e) => {
-    operation = e.target.value; //???
+    if (e.target.value === "=" || e.target.value === ".") {
+      operatorClicked === false;
+    } else operatorClicked = true;
+
+    if (e.target.value === "=" && operatorClicked != false) {
+      display.value =
+        numOne + " " + `${operator}` + " " + numTwo + " " + "=" + " " + result;
+    }
+    operator = e.target.value;
+
+    //console.log(operator);
   });
 });
