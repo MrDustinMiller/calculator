80f768261877d0d6712dcd72df603ca2ecb8ceed
MrDustinMiller <dustin.miller@mscoding.org>
2022-11-16 14:44:46 -0600

    finished functionality

--- scripts/script.js
+++ scripts/script.js
@@ -2,138 +2,238 @@ let display = document.querySelector(".display");
 let numbers = document.querySelectorAll(".num");
 let operations = document.querySelectorAll(".operations");
 let deleteBtns = document.querySelectorAll(".delete-buttons");
+let equals = document.querySelector(".equals");
 let operatorClicked = false;
+let equalClicked = false;
+let continuedOperation = false;
 let operator;
-let numOne = 0;
-let numTwo = 0;
-let result = 0;
+let numberOne;
+let numberTwo;
+let result;
+let array = new Array;
 
-let clearScreen = () => {
-  display.value = "";
-  operatorClicked = false;
-  numOne = 0;
-  numTwo = 0;
-  result = 0;
-  operator = "";
-};
-
-let backSpace = () => {
-  let array = Array.from(display.value);
-  array.pop();
-
-  let output = array.join("");
-  display.value = output.toString();
-
-  if (array.length === 0) {
+  let clearScreen = () => {
     display.value = "";
+    operatorClicked = false;
+    equalClicked = false;
+    continuedOperation = false;
+    numberTwo = 0;
+    numberOne = 0;
+    result = undefined;
+    operator = ""
+  };
+
+  let backSpace = () => {
+    let array = Array.from(display.value);
+    array.pop();
+    let output = array.join("");
+    display.value = output.toString();
+  
+    if (array.length === 0) {
+      display.value = "";
+    }
+  };
+  
+  let addition = (numOne, numTwo) => {
+    result = numOne + numTwo;
+    display.value += " " + "=" + " " + result;
+      if (continuedOperation === true) {
+          display.value = result;
+        clearValuesOnChainedEquation();
+      }
+    array = [];
+  };
+  
+  let subtraction = (numOne, numTwo) => {
+    result = numOne - numTwo;
+    display.value += " " + "=" + " " + result;
+    if (continuedOperation === true) {
+        display.value = result;
+        clearValuesOnChainedEquation();
+    }
+    array = [];
+  };
+  
+  let multiplication = (numOne, numTwo) => {
+    result = numOne * numTwo;
+    display.value += " " + "=" + " " + result;
+    if (continuedOperation === true) {
+        display.value = result;
+        clearValuesOnChainedEquation();
+    }
+    array = [];
+  };
+  
+  let division = (numOne, numTwo) => {
+    result = numOne / numTwo;
+    display.value += " " + "=" + " " + result;
+    if (continuedOperation === true) {
+        display.value = result;
+        clearValuesOnChainedEquation();
+    }
+    array = [];
+  };
+  
+  let operate = (operator, numOne, numTwo) => {
+    if (numOne !== 0 && numTwo !== 0) {
+        switch (operator) {
+      case "+":
+        addition(numOne, numTwo);
+        break;
+      case "-":
+        subtraction(numOne, numTwo);
+        break;
+      case "*":
+        multiplication(numOne, numTwo);
+        break;
+      case "/":
+        division(numOne, numTwo);
+        break;
+  
+      default :
+        break;
+    }
+    } else return;
+    
+  };
+
+  let displayValues = (e) => { 
+    let newOperand;   
+
+    //for continued equations
+    if (result !== undefined && operator !== undefined) {
+        //push each target value to an array
+        array.push(e.target.value);
+        newOperand = array.join("");
+        //once values are joined together this will be our new operand
+        display.value =  result + `${operator}` + newOperand;
+    } 
+
+    //for the first equation
+    if (continuedOperation === false) {
+         display.value += e.target.value;
+    } 
   }
-};
 
-let addition = (numOne, numTwo) => {
-  result = numOne + numTwo;
-};
-
-let subtraction = (numOne, numTwo) => {
-  result = numOne - numTwo;
-};
-
-let multiplication = (numOne, numTwo) => {
-  result = numOne * numTwo;
-  //   let nums = Array.from(array);
-  //   let result = nums.reduce((accumulator, nextItem) => accumulator * nextItem);
-  //   console.log(array);
-  //   display.value = result;
-};
-
-let division = (numOne, numTwo) => {
-  result = numOne / numTwo;
-};
-
-let operate = (operator, numOne, numTwo) => {
-  switch (operator) {
-    case "+":
-      addition(numOne, numTwo);
-      break;
-    case "-":
-      subtraction(numOne, numTwo);
-      break;
-    case "*":
-      multiplication(numOne, numTwo);
-      break;
-    case "/":
-      division(numOne, numTwo);
-      break;
-
-    default:
-      break;
+  let clearValuesOnChainedEquation = () => {
+    if (continuedOperation === true && equalClicked === true) {
+        numberOne = 0;
+        numberTwo = 0;
+    }
   }
-};
 
-function test() {
-  //make sure we have to numbers to do an expression with
-  if (numOne && numTwo > 0) {
-    operate(operator, numOne, numTwo);
+  let checkForNextClick = (numOne, numTwo) => {
+    //any operation with equals clicked to get result
+    if (equalClicked === true) {
+        operate(operator, numOne, numTwo)
+    } 
+
+    //any operation with continued operator clicked to get result
+    if (continuedOperation === true) {
+        operate(operator, numOne, numTwo)
+    }
   }
+
+  let getFirstNum = () => { 
+    numberOne = Number(display.value);    
+  }
+
+ let getSecondNum = () => {     
+    let test = Array.from(display.value)
+    let index = test.indexOf(operator)
+    let numTwoArray = test.splice(index+1)
+    //join the array together to have our numTwo
+    numberTwo = numTwoArray.join("")
+    numberTwo = Number(numberTwo)   
+ }
+
+ let checkForFullEquation = (e) => {
+    //error msg
+    if (e.target.value === "=" && numberTwo === 0 && operator != "/") {
+        alert(
+          "Please select a operation and a second number before hitting equals"
+        );
+      }
+ }
+
+let checkForDivideByZero = () => {
+    //divide by zero error message
+    if (operatorClicked != false && operator === "/" && numberTwo === 0) {
+        alert("You can't divide by 0 silly");
+        if (result === undefined) {
+            //for first equation
+            display.value = numberOne + `${operator}`;
+            //for any equation after
+        } else display.value = result + `${operator}`;
+      }
 }
 
-numbers.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    display.value += e.target.value;
-
-    if (operatorClicked != true) {
-      numOne += Number(e.target.value);
+let checkForContinuedOperation = (result, newOperand) => {
+  //any equation after the first one
+   if (continuedOperation === true) {
+        checkForNextClick(result, newOperand)
+    } 
+    
+   //this would be the first pass through an equation by clicking another operator as opposed to equals "="
+    if (numberOne !== undefined && numberTwo !== undefined && operator !== undefined && result === undefined) {
+        if (numberTwo === 0) {
+            display.value = numberOne + `${operator}`
+        } else if (numberTwo !== 0 ) {
+             continuedOperation = true;
+            checkForNextClick(numberOne, numberTwo)
+        } 
     }
+};
+ 
+ //start with an empty calculator screen on page load
+ clearScreen();
 
-    //if operatorClicked = true then add the inputted values to our second num, not the first.
-    if (numOne > 0 && operatorClicked != false) {
-      numTwo += Number(e.target.value);
-      display.value = numOne + `${operator}` + numTwo;
-    }
+  numbers.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      displayValues(e);  
+      if (operatorClicked !== true) {
+        getFirstNum();
+      } else getSecondNum();
 
-    //divide by zero error message
-    if (operatorClicked != false && operator === "/" && numTwo === 0) {
-      display.value = numOne + `${operator}`;
-      alert("You can't divide by 0 silly");
-    }
-
-    test();
+      checkForDivideByZero();
+    });
   });
-});
 
-deleteBtns.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    if (e.target.innerHTML === "CLEAR") {
-      clearScreen();
-    } else if (e.target.innerHTML == "BACKSPACE") {
-      backSpace();
-    }
+  deleteBtns.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      if (e.target.innerHTML === "CLEAR") {
+        clearScreen();
+      } else if (e.target.innerHTML == "BACKSPACE") {
+        backSpace();
+      }
+    });
   });
-});
 
-operations.forEach((element) => {
-  element.addEventListener("click", (e) => {
-    //no operator had been clicked yet, only equals or decimal has
-    if (e.target.value === "=" || e.target.value === ".") {
-      operatorClicked === false;
-    } else if ((operatorClicked = true)) {
-      //has to be an operator clicked if its not "="/".". set operatorClicked to TRUE
-      operator = e.target.value;
-      display.value += `${operator}`;
-    }
+  equals.addEventListener("click", (e) => {
+    checkForFullEquation(e);
+    equalClicked = true;
+    
+    //first equation
+    if (result === undefined) {
+        checkForNextClick(numberOne, numberTwo)
+        //any equation after
+    } else checkForNextClick(result,numberTwo);
 
-    //display error msg if user clicks a operand and operator but no second operand.
-    //extra operator != condition so we can differeniate between equal message and
-    //divide by zero message.
-    if (e.target.value === "=" && numTwo === 0 && operator != "/") {
-      alert(
-        "Please select operation and a second number before hitting equals"
-      );
-    }
+  })
 
-    //if numTwo is equal to 0 then we have no final equation to display
-    if (e.target.value === "=" && operatorClicked != false && numTwo != 0) {
-      display.value =
-        numOne + " " + `${operator}` + " " + numTwo + " " + "=" + " " + result;
-    }
+  operations.forEach((element) => {
+    element.addEventListener("click", (e) => {
+    operator = e.target.value;   
+
+    //this will stop being able to input multiple operators on chained equations
+    if (numberOne === 0 && numberTwo === 0 && continuedOperation === true) {
+        display.value = result + `${operator}`;
+    }else display.value += `${operator}`;
+
+    operatorClicked = true; 
+
+    if (continuedOperation === false) {
+        checkForContinuedOperation();
+    } else checkForNextClick(result,numberTwo)   
+    });
   });
-});
